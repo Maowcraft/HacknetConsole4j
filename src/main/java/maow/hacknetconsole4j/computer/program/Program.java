d4cf9a4d2fa1b39e6fc89dfd31312d536ec554f1
Maow <28311382+Maowcraft@users.noreply.github.com>
2020-09-24 19:06:12 -0400

    Delete Program.java

--- src/main/java/maow/hacknetconsole4j/computer/program/Program.java
+++
@@ -1,8 +0,0 @@
-package maow.hacknetconsole4j.computer.program;
-
-public interface Program {
-    String getName();
-    int getTimeRunning();
-    boolean isBuiltIn();
-    void run(String[] args);
-}
